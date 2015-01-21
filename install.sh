cd406b1fb64a0fc82aa8648da067783adb61f8d3
tainzhi <qfq61@qq.com>
2015-01-21 12:58:12 +0800

    1. create soft link 2. add install.sh to install simplily 3. add uninstall.sh to remove soft link

---
+++ install.sh
@@ -0,0 +1,13 @@
+# shell
+ln ./config_sh/zshrc ~/.zshrc
+ln ./config_sh/bashrc ~/.bashrc
+
+# git
+ln ./config_sh/gitignore ~/.gitignore
+ln ./config_sh/gitconfig ~/.gitconfig
+
+# tmux
+ln ./config_sh/tmux.conf ~/.tmux.conf
+
+# for AndroidStudio or Intellij IDEA plugin IdeaVim
+ln ./config_sh/ideavimrc ~/.ideavimrc
---
+++ unistall.sh
@@ -0,0 +1,13 @@
+# shell
+rm -rf ~/.zshrc
+rm -rf ~/.bashrc
+
+# git
+rm -rf ~/.gitignore
+rm -rf ~/.gitconfig
+
+# tmux
+rm -rf ~/.tmux.conf
+
+# for AndroidStudio or Intellij IDEA plugin IdeaVim
+rm -rf ~/.ideavimrc
