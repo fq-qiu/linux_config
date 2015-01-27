5436c16b5cea9ebd71461efcb436599314097760
qiufuqing <qiufuqing@bbktel.com>
2015-01-28 06:42:46 +0800

    add tags.sh which creates tags and cscope for vim

--- shell_tools/tags.sh
+++ shell_tools/tags.sh
@@ -1,6 +1,6 @@
 find -iname "*.java" > ./cscope.files
 find -iname "*.xml" >> ./cscope.files
-"find . -name "*.h" -o -name "*.c" -o -name "*.cc" > cscope.files
+#find . -name "*.h" -o -name "*.c" -o -name "*.cc" > cscope.files
 
 ctags --fields=+i -n -R -L ./cscope.files
 
