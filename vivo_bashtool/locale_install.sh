b50a30ba95a4f880e0f90041ad75c255fd0a9202
tainzhi <qfq0601@gmail.com>
2015-05-27 08:58:16 +0800

    修改adb shell kill和vivo安装apk的脚本

--- vivo_bashtool/locale_install.sh
+++ vivo_bashtool/locale_install.sh
@@ -1,5 +1,3 @@
-# 读取.compile_tmp文件获取android版本, app, 然后push到android设备种安装
- 
 CURRENT_PATH=`pwd`
 TAG="____"
 COMMON_APPLICATION=("BBKVideoPlayer" "UpnpServer" "AudioEffect" "FlaotingWindow")
@@ -13,26 +11,47 @@ APPLICATION=${APPLICATION%.*}
 echo ${TAG}"sdk_version____:"${ANDROID_VERSION}
 echo ${TAG}"application____:"${APPLICATION}
 
-target_install_path="system/app"
-for i in "${SYSTEM_APPLICATION[@]}"; do
-    if [ $i = $APPLICATION ]
-    then
-        target_install_path="system/priv-app"
-    fi
-done
-echo ${TAG}"target_path____:"${target_install_path}
+PACKAGENAME=${APPLICATION}
+TARGET_INSTALL_PATH="system/app"
+if [[ ${APPLICATION} = "BBKVideoPlayer" ]]; then
+    echo ${APPLICATION}
+    PACKAGENAME="VideoPlayer"
+elif [[ ${APPLICATION} = "MediaProvider" ]]; then
+    echo ${APPLICATION}
+    TARGET_INSTALL_PATH="system/priv-app"
+fi 
 
-if [ ${ANDROID_VERSION:0:1} = "5" ]
-then
-    echo "adb push "${APPLICATION_ORIGINAL_PATH}" "${target_install_path}"/"${APPLICATION}"/"
-    adb push ${APPLICATION_ORIGINAL_PATH} ${target_install_path}"/"${APPLICATION}"/"
-    #sleep 3s
-    zygote_id=`adb shell ps | grep -i zygote | awk '{print $2}'`
-    adb shell kill -9 $zygote_id
-    #adb shell am broadcast -a android.intent.action.BOOT_COMPLETED
-else
-    echo "adb push "${APPLICATION_ORIGINAL_PATH}" "${target_install_path}"/"
-    adb shell rm ${target_install_path}"/"${APPLICATION}"*"
-    adb push ${APPLICATION_ORIGINAL_PATH} ${target_install_path}"/"
+if [ ${ANDROID_VERSION:0:1} = "5" ]; then
+    TARGET_INSTALL_PATH=${TARGET_INSTALL_PATH}"/"${APPLICATION}
 fi
+echo ${TAG}"target_path____:"${TARGET_INSTALL_PATH}
+
+# 判断是否remount成功, 如果失败, 那么exit status $? 为1, 然后vivoroot, 等待2s, 使得root成功
+# remount_status=$(adb remount)
+adb remount | grep succeeded
+if [[ $? -eq 1 ]]; then
+    adb vivoroot
+    sleep 2s
+    adb remount
+fi
+
+
+if [[ ${APPLICATION} = "MediaProvider" ]]; then
+    echo "adb push "${APPLICATION_ORIGINAL_PATH}" "${TARGET_INSTALL_PATH}"/"${APPLICATION}
+    adb shell rm ${TARGET_INSTALL_PATH}"/"${APPLICATION}"*"
+    adb push ${APPLICATION_ORIGINAL_PATH} ${TARGET_INSTALL_PATH}"/"
+
+    adb shell rm -rf data/data/com.android.providers.media
+    adb shell am broadcast -a android.intent.action.MEDIA_MOUNTED -d file:///storage/sdcard0
+    adb shell am broadcast -a android.intent.action.MEDIA_MOUNTED -d file:///storage/sdcard1
+    #adb shell am braodcast -a android.intent.action.BOOT_COMPLETED
+else
+    adb shell ps | grep -i ${PACKAGENAME} | awk '{print $9}' | xargs adb shell am force-stop
+    echo "adb push "${APPLICATION_ORIGINAL_PATH}" "${TARGET_INSTALL_PATH}"/"${APPLICATION}
+    adb shell rm ${TARGET_INSTALL_PATH}"/"${APPLICATION}"*"
+    adb push ${APPLICATION_ORIGINAL_PATH} ${TARGET_INSTALL_PATH}"/"
+fi
+
+
+
 
