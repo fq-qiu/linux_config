b2518459966a7d9e2845c48ef0158329125aa945
qiufuqing <qfq0601@gmail.com>
2015-09-14 15:35:07 +0800

    modify vivo bashtool

--- vivo_bashtool/plus_revision.sh
+++ vivo_bashtool/plus_revision.sh
@@ -2,7 +2,7 @@ recent=("1" "2" "3" "4" "5")
 for i in ${recent[@]}
 do
     git log --oneline --decorate --color| sed -n "${i}p"
-    prefix="[git:"$(repository)"|branch:"$(remote_branch)"|commit_id:"$(git log --oneline --decorate | sed -n "${i}p" | awk '{print $1}')"]"
+    prefix="[git:VivoCode/"$(repository)"|branch:"$(remote_branch)"|commit_id:"$(git log --oneline --decorate | sed -n "${i}p" | awk '{print $1}')"]"
     echo ${prefix}
     echo "*********************************************************************"
 done
