6c3124d0c850586379cd5e9129d80f4d64b83fba
qiufuqing <qiufuqing@bbktel.com>
2016-03-03 09:50:49 +0800

    some modifications for scsope and gitignore

--- shell_tools/tags.sh
+++ shell_tools/tags.sh
@@ -1,6 +1,6 @@
 CURRENT_PATH=`pwd`
 
-find $CURRENT_PATH -iname "*.java" -o -name "*.xml" -o -name "*.h"  -o -name "*.c" -o -name "*.cpp" -o -name "*.cc" | grep -v .plus > ./cscope.files
+find $CURRENT_PATH -iname "*.java" -o -name "*.xml" -o -name "*.h"  -o -name "*.c" -o -name "*.cpp" -o -name "*.cc" -o -name "*.aidl" | grep -v .plus > ./cscope.files
 
 # find $CURRENT_PATH -iname "*.xml" >> ./cscope.files
 # find $CURRENT_PATH  -name "*.h" -o -name "*.c" -o -name "*.cc"  -o -name "*.cpp" >> cscope.files
