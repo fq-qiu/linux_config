eb52c2945dc3d9ca59d909ece7264414b58bd542
tainzhi <qfq61@qq.com>
2021-03-17 15:58:20 +0800

    update config: add DIR for ln

--- config.sh
+++ config.sh
@@ -1,45 +1,31 @@
 # bash shell
 # 先备份原有的.bashrc, 再使用自定义的
 # ln -s source_file target_file
+DIR="$( cd "$( dirname "$0" )" && pwd )"
 
-if [ ! -L ~/.bashrc ]; then
-    mv ~/.bashrc ~/.bashrc_backup
-fi
-ln -s ./config_sh/bashrc ~/.bashrc
-if [ ! -L ~/.bash_history ]; then
-    mv ~/.bash_history ~/.bash_history_backup
-fi
-ln -s ./config_sh/bash_history ~/.bash_history
+# bash
+ln -f -s $DIR/config_sh/bashrc ~/.bashrc
+ln -f -s $DIR/config_sh/bash_history ~/.bash_history
 
 # zsh
-# 先备份原有的.zshrc, 再使用自定义的
-if [ ! -L ~/.zshrc ]; then
-    mv ~/.zshrc ~/.zshrc_backup
-fi
-ln -s ./config_sh/zshrc ~/.zshrc
-if [ ! -L ~/.zsh_history ]; then
-    mv ~/.zsh_history ~/.zsh_history_backup
-fi
-ln -s ./config_sh/zsh_history ~/.zsh_history
-ln -s ./oh-my-zsh ~/.oh-my-zsh
+ln -f -s $DIR/config_sh/zshrc ~/.zshrc
+ln -f -s $DIR/config_sh/zsh_history ~/.zsh_history
+ln -f -s $DIR/oh-my-zsh ~/.oh-my-zsh
 
 # git
-ln -s ./config_sh/gitignore ~/.gitignore
-ln -s ./config_sh/gitconfig ~/.gitconfig
-ln -s ./config_sh/git.init.templateDir ~/.git.init.templateDir
+ln -f -s $DIR/config_sh/gitignore ~/.gitignore
+ln -f -s $DIR/config_sh/gitconfig ~/.gitconfig
+ln -f -s $DIR/config_sh/git.init.templateDir ~/.git.init.templateDir
 
 # tmux
-if [ ! -d ~/.tmux ]; then
-    ln -s ./tmux ~/.tmux
-fi
-ln -s ./config_sh/tmux.conf ~/.tmux.conf
+ln -f -s $DIR/config_sh/tmux.conf ~/.tmux.conf
 
 # for AndroidStudio or Intellij IDEA plugin IdeaVim
-ln -s ./config_sh/ideavimrc ~/.ideavimrc
+ln -f -s $DIR/config_sh/ideavimrc ~/.ideavimrc
 
 # for vim
 if [ ! -d ~/.vim ]; then
-    ln -s ./vimfile ~/.vim
+    ln -f -s $DIR/vimfile ~/.vim
     cd ~/.vim
     echo | vim -u ~/.vim/vimrc +BundleUpdate +qall
 fi
@@ -49,14 +35,14 @@ fi
 
 # for ssh
 mkdir ~/.ssh
-ln -s ./config_sh/ssh-config ~/.ssh/config
+ln -f -s $DIR/config_sh/ssh-config ~/.ssh/config
 # cd ~/.ssh
 # yes | ssh-keygen -P "" -t rsa -b 4096 -f ~/.ssh/github.com
 # yes | ssh-keygen -P "" -t rsa -f ~/.ssh/coding.net
 chmod 600 ~/.ssh/*
 
 # for show markdown toc in vim tagbar
-ln -s ./config_sh/ctags ~/.ctags
+ln -f -s $DIR/config_sh/ctags ~/.ctags
 
 
 # for iptables, block some online game show sites
@@ -73,9 +59,9 @@ sed -i '$a ZIPINFO = "-O CP936"' /etc/environment
 
 # pip使用国内镜像源
 mkdir -p ~/.pip/
-ln -s ./config_sh/pip.conf ~/.pip/pip.confg
+ln -f -s $DIR/config_sh/pip.conf ~/.pip/pip.confg
 
 
 # fonts
-ln -s ./fonts ~/.fonts
+ln -f -s $DIR/fonts ~/.fonts
 fc-cache -vf
