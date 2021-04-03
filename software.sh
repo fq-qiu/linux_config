a264be056c41723acf8853a62a5cabf814a7897b
tainzhi <qfq61@qq.com>
2021-04-03 08:19:42 +0800

    macOS: install z.lua as git submodule

--- software.sh
+++ software.sh
@@ -1,51 +1,59 @@
-# build essential
-apt-get -y install g++
-apt-get -y install build-essential
-apt-get -y install automake libevent-dev libncurses5-dev libncurses5-dev
-apt-get -y install curl
-apt-get -y install cmake python-dev python-setuptools python3-dev python3-pip
-yes | pip3 install --upgrade pip
-apt-get -y remove python3-pip
-
-# zsh
-apt-get -y install zsh
-apt-get -y install xclip
-# chsh -s /bin/zsh
-# sed -i "s/$USER\:\/bin\/bash/$USER\:\/bin\/zsh/g" /etc/passwd
-# just start the terminal and use zsh
-# /usr/bin/env zsh
+DIR="$( cd "$( dirname "$0" )" && pwd )"
+OS=$(uname -s)
 
 
-# replace autojump with z.lua
-apt-get -y install lua5.3
-git clone https://github.com/skywind3000/z.lua ~/
+if [ ${OS} = "Darwin" ]; then
+    homebrew install lua
+elif [ ${OS} = "Linux"  ]; then
+    # build essential
+    apt-get -y install g++
+    apt-get -y install build-essential
+    apt-get -y install automake libevent-dev libncurses5-dev libncurses5-dev
+    apt-get -y install curl
+    apt-get -y install cmake python-dev python-setuptools python3-dev python3-pip
+    yes | pip3 install --upgrade pip
+    apt-get -y remove python3-pip
 
-# tmux
-apt-get -y install tmux
+    # zsh
+    apt-get -y install zsh
+    apt-get -y install xclip
+    # chsh -s /bin/zsh
+    # sed -i "s/$USER\:\/bin\/bash/$USER\:\/bin\/zsh/g" /etc/passwd
+    # just start the terminal and use zsh
+    # /usr/bin/env zsh
 
-# pycscope
-pip3 install pycscope
-# vim
-apt-get -y install vim-gnome
-# vim denpendency
-apt-get -y install ctags cscope
-# pycscope
-pip3 install pycscope
+    # replace autojump with z.lua
+    apt-get -y install lua5.3
+    git clone https://github.com/skywind3000/z.lua ~/
 
-## rg search
-## if linux version >= 18.10, ripgrep can be installed by apt-install
-## others, it can only be installed mannually
-apt-get -y install ripgrep || curl -LO https://github.com/BurntSushi/ripgrep/releases/download/11.0.1/ripgrep_11.0.1_amd64.deb && dpkg -i ripgrep_11.0.1_amd64.deb
+    # tmux
+    apt-get -y install tmux
 
-## better than find
-apt-get -y fd-find
+    # pycscope
+    pip3 install pycscope
+    # vim
+    apt-get -y install vim-gnome
+    # vim denpendency
+    apt-get -y install ctags cscope
+    # pycscope
+    pip3 install pycscope
 
-#p7zip-full
-# apt-get -y install p7zip-full
+    ## rg search
+    ## if linux version >= 18.10, ripgrep can be installed by apt-install
+    ## others, it can only be installed mannually
+    apt-get -y install ripgrep || curl -LO https://github.com/BurntSushi/ripgrep/releases/download/11.0.1/ripgrep_11.0.1_amd64.deb && dpkg -i ripgrep_11.0.1_amd64.deb
 
-# Node.js, npm
-# apt-get -y install Nodejs npm
+    ## better than find
+    apt-get -y fd-find
 
-# compatible i386, for android mksdard tool
-# dpkg --add-architecture i386
-# apt-get -y install libncurses5:i386 libstdc++6:i386 zlib1g:i386 lib32z1 lib32ncurses5
+    #p7zip-full
+    # apt-get -y install p7zip-full
+
+    # Node.js, npm
+    # apt-get -y install Nodejs npm
+
+    # compatible i386, for android mksdard tool
+    # dpkg --add-architecture i386
+    # apt-get -y install libncurses5:i386 libstdc++6:i386 zlib1g:i386 lib32z1 lib32ncurses5
+elif [ ${OS} = "MINGW32_NT" ]; then
+fi
